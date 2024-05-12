a49a895989b0c35ccccc3b45c265a37fdcd2d503
mhnaufal <aal.pirates16@gmail.com>
2024-05-12 11:28:54 +0700

    [7] edit comments

--- ChiliWin.h
+++ ChiliWin.h
@@ -23,9 +23,9 @@
 // target Windows 7 or later
 #define _WIN32_WINNT 0x0601
 #include <sdkddkver.h>
-// The following #defines disable a bunch of unused windows stuff. If you 
-// get weird errors when trying to do some windows stuff, try removing some
-// (or all) of these defines (it will increase build time though).
+#define NOTEXTMETRIC
+*/
+
 #define WIN32_LEAN_AND_MEAN
 #define NOGDICAPMASKS
 #define NOSYSMETRICS
@@ -43,12 +43,10 @@
 #define NONLS
 #define NOMEMMGR
 #define NOMETAFILE
-#define NOMINMAX
 #define NOOPENFILE
 #define NOSCROLL
 #define NOSERVICE
 #define NOSOUND
-#define NOTEXTMETRIC
 #define NOWH
 #define NOCOMM
 #define NOKANJI
@@ -60,9 +58,8 @@
 #define NOPROXYSTUB
 #define NOIMAGE
 #define NOTAPE
-
 #define STRICT
-*/
+#define NOMINMAX
 
 /*
 * Include files
@@ -72,9 +69,9 @@
 #include <windowsx.h>
 
 // DirectX11
-#include <d3d11.h> // Direct3D 11 main library (mandatory)
-#include <d3dx11.h> // Direct3D 11 extension library (optional)
-#include <d3dx10.h> // Direct3D 10 extension library (optional)
+#include <d3d11.h>		// Direct3D 11 main library (mandatory)
+#include <d3dx11.h>		// Direct3D 11 extension library (optional)
+#include <d3dx10.h>		// Direct3D 10 extension library (optional)
 
 #pragma comment (lib, "d3d11.lib") // include the Direct3D 11 lib to our executable
 #pragma comment (lib, "d3dx11.lib") 
@@ -83,31 +80,35 @@
 // STL
 #include <iostream>
 
-// global declarations
+/* 
+* Global Declarations
+*/
+auto g_window_name = (LPCSTR)"3DirectXNihBos";
 IDXGISwapChain* swapchain;					// the pointer to the swap chain interface
 ID3D11Device* dev;							// the pointer to our Direct3D device interface
 ID3D11DeviceContext* devcon;				// the pointer to our Direct3D device context (physical device)
-auto wind_name_g = (LPCSTR)"3DirectXNihBos";
 ID3D11RenderTargetView* backBuffer;			// variable that holds information about the render target
 ID3D11VertexShader* pVS;					// COM object for vertex shader
 ID3D11PixelShader* pPS;						// COM object for pixel shader
-ID3D11Buffer* pVBuffer;                      // vertex buffer
+ID3D11Buffer* pVBuffer;						// vertex buffer
 ID3D11InputLayout* pLayout;
 
-// function prototypes
-void InitDX3D(HWND hWnd);     // sets up and initializes Direct3D
-void CleanDX3D(void);         // closes Direct3D and releases memory
-void RenderFrame(void);
-void InitPipeline(void);
-void InitGraphic(void);
+constexpr auto SCREEN_WIDTH = 800;
+constexpr auto SCREEN_HEIGHT = 600;
 
-#define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 600
-
-// Vertex data
 struct VERTEX
 {
 	FLOAT X, Y, Z;
 	D3DXCOLOR Color;
 };
+
+/* 
+* Function prototypes
+*/
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
+void InitDX3D(HWND hWnd);     // sets up and initializes Direct3D
+void CleanDX3D(void);         // closes Direct3D and releases memory
+void RenderFrame(void);
+void InitPipeline(void);
+void InitGraphic(void);
 
--- WinMain.cpp
+++ WinMain.cpp
@@ -1,26 +1,115 @@
 #include "ChiliWin.h"
 
-/*
-* Window procedure
+/* NOTE:
+* https://learn.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-winmain
 */
+int CALLBACK WinMain(
+	HINSTANCE hInstance,		/* NOTE: context in vulkan, contains data of current window program */
+	HINSTANCE hPrevInstance,	/* NOTE: old API, always nullptr now */
+	LPSTR lpCmdLine,			/* NOTE; command line param that we can pass through window */
+	int nCmdShow				/* NOTE: command show option */
+) {
+	/* NOTE:
+	* Register window class to this program. Remember this is a "class"
+	* https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-registerclassexa
+	* https://learn.microsoft.com/en-us/windows/win32/api/winuser/ns-winuser-wndclassexa
+	*/
+	const LPCSTR pClassName = "3DirectXNihBos";
+	WNDCLASSEX wc{ 0 };
+	wc.cbSize = sizeof(wc);				/* NOTE: sizeof this struct */
+	wc.style = CS_OWNDC;				/* NOTE: class style enum */
+	//wc.lpfnWndProc = DefWindowProc;	/* NOTE pointer to window procedure that this class want to use */
+	wc.lpfnWndProc = WndProc;			/* NOTE: window procedure is just what the window do when it's running */
+	wc.cbClsExtra = 0;					/* NOTE: extra byte we want to allocate */
+	wc.cbWndExtra = 0;					/* NOTE: number of extra byte allocated */
+	wc.hInstance = hInstance;			/* NOTE: instance of this window program */
+	wc.hbrBackground = nullptr;			/* NOTE: background color */
+	wc.hCursor = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.hIcon = nullptr;					/* NOTE: window icon that we want to use */
+	wc.hIconSm = nullptr;				/* small icon that we use */
+	wc.lpszClassName = pClassName;		/* the window class name that we use */
+
+	RegisterClassEx(&wc);
+
+	/* NOTE:
+	* Create a window based on the window class that we use
+	* https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-createwindowexa
+	*/
+	HWND hwnd = CreateWindowEx(
+		0,
+		pClassName,									/* NOTE: which window class it is */
+		g_window_name,								/* NOTE: window name */
+		WS_MINIMIZEBOX | WS_SYSMENU | WS_CAPTION,	/* NOTE: window style that we want to use */
+		300, 300,									/* NOTE: window position */
+		SCREEN_WIDTH, SCREEN_HEIGHT,				/* NOTE: window size */
+		nullptr,
+		nullptr,
+		hInstance,
+		nullptr);
+
+	/* NOTE:
+	* Show the window that we just made
+	* https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-showwindow
+	*/
+	ShowWindow(
+		hwnd,
+		SW_SHOW /* NOTE: enum how we want to show it */
+	);
+
+	/* NOTE:
+	* Initialize Directx3D 
+	*/
+	InitDX3D(hwnd);
+
+	InitPipeline();
+	InitGraphic();
+
+	/* NOTE:
+	* Looping the event message between the window and program
+	* https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getmessage
+	*/
+	BOOL gResult{};
+	MSG message{};							/* NOTE: Windows message struct */
+	while (gResult = GetMessage(&message, nullptr, 0, 0) != 0) {
+		if (gResult == -1) {
+			return -1;
+		}
+
+		TranslateMessage(&message);			/* NOTE: translate virtual key message into character message */
+		DispatchMessage(&message);
+
+		RenderFrame();
+	}
+
+	/* NOTE: Deinitialize DirectX3D */
+	CleanDX3D();
+
+	return (int)message.wParam; // 313
+}
+
+
+/////////////////////////////
+// Windows callback procedure
+/////////////////////////////
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 {
 	switch (msg)
 	{
 	case WM_CLOSE:
-		PostQuitMessage(313); // tell the window to close the program when 'X' button is clicked
+		PostQuitMessage(313);		/* NOTE: tell the window to close the program when 'X' button is clicked */
 		break;
-	case WM_KEYDOWN: // keystroke input
+	case WM_KEYDOWN:				/* NOTE: keystroke input */
 		if (wparam == 'F') {
 			SetWindowText(hwnd, (LPCSTR)"New Window Name");
 		}
 		break;
 	case WM_KEYUP:
 		if (wparam == 'F') {
-			SetWindowText(hwnd, wind_name_g);
+			SetWindowText(hwnd, g_window_name);
 		}
 		break;
-	case WM_CHAR: // sequence of word input
+	case WM_CHAR:					/* NOTE: sequence of word input */
 		break;
 	case WM_LBUTTONDBLCLK:
 		int x_pos{}, y_pos{};
@@ -33,144 +122,61 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 	return DefWindowProc(hwnd, msg, wparam, lparam);
 }
 
-/*
-* https://learn.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-winmain
-*/
-int CALLBACK WinMain(
-	HINSTANCE hInstance, // context in vulkan, contains data of current window program
-	HINSTANCE hPrevInstance, // old API, always nullptr now
-	LPSTR lpCmdLine, // command line param that we can pass through window
-	int nCmdShow // command show option
-) {
-	/*
-	* Register window class to this program. Remember this is a "class"
-	* https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-registerclassexa
-	* https://learn.microsoft.com/en-us/windows/win32/api/winuser/ns-winuser-wndclassexa
-	*/
-	const LPCSTR pClassName = "3DirectXNihBos";
-	WNDCLASSEX wc{ 0 };
-	wc.cbSize = sizeof(wc); // sizeof this struct
-	wc.style = CS_OWNDC; // class style enum
-	//wc.lpfnWndProc = DefWindowProc; // pointer to window procedure that this class want to use
-	wc.lpfnWndProc = WndProc; // window procedure is just what the window do when it's running
-	wc.cbClsExtra = 0; // extra byte we want to allocate
-	wc.cbWndExtra = 0; // number of extra byte allocated
-	wc.hInstance = hInstance; // instance of this window program
-	wc.hbrBackground = nullptr; // background color
-	wc.hCursor = nullptr;
-	wc.lpszMenuName = nullptr;
-	wc.hIcon = nullptr; // window icon that we want to use
-	wc.hIconSm = nullptr; // small icon that we use
-	wc.lpszClassName = pClassName; // the window class name that we use
 
-	RegisterClassEx(&wc);
-
-	/*
-	* Create a window based on the window class that we use
-	* https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-createwindowexa
-	*/
-	HWND hwnd = CreateWindowEx(
-		0,
-		pClassName, // which window class it is
-		wind_name_g, // window name
-		WS_MINIMIZEBOX | WS_SYSMENU | WS_CAPTION, // window style that we want to use
-		300, 300, // window position
-		SCREEN_WIDTH, SCREEN_HEIGHT, // window size
-		nullptr,
-		nullptr,
-		hInstance,
-		nullptr);
-
-	/*
-	* Show the window that we just made
-	* https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-showwindow
-	*/
-	ShowWindow(
-		hwnd,
-		SW_SHOW // enum how we want to show it
-	);
-
-	/* Initialize Directx3D */
-	InitDX3D(hwnd);
-
-	InitPipeline();
-	InitGraphic();
-
-	/*
-	* Looping the event message between the window and program
-	* https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getmessage
-	*/
-	BOOL gResult{};
-	MSG message{}; // Windows message struct
-	while (gResult = GetMessage(&message, nullptr, 0, 0) != 0) {
-		if (gResult == -1) {
-			return -1;
-		}
-
-		TranslateMessage(&message); // translate virtual key message into character message
-		DispatchMessage(&message);
-
-		RenderFrame();
-	}
-
-	/* Deinitialize DirectX3D */
-	CleanDX3D();
-
-	return message.wParam; // 313
-}
-
-/*
-* Initialize the DirectX 3D 11
-*/
+////////////////////////////
+// Initialize DirectX 11
+///////////////////////////
 void InitDX3D(HWND hWnd)
 {
-	DXGI_SWAP_CHAIN_DESC swc_struct{}; // struct holds information about swap chain
+	DXGI_SWAP_CHAIN_DESC swc_struct{}; /* NOTE: struct holds information about swap chain */
 
 	ZeroMemory(&swc_struct, sizeof(DXGI_SWAP_CHAIN_DESC));
 
-	// fill the swap chain struct. 
-	// https://learn.microsoft.com/en-us/windows/win32/api/dxgi/ns-dxgi-dxgi_swap_chain_desc
-	// https://learn.microsoft.com/en-us/windows/win32/direct3ddxgi/dxgi-usage
-	swc_struct.BufferCount = 1;									// one back buffer & one front buffer (in pair)
-	swc_struct.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;	// color format used. Use 32 bit color
-	swc_struct.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;	// swap chain behaviour flags. How swap chain to be used
-	swc_struct.OutputWindow = hWnd;								// which window context to be used
-	swc_struct.SampleDesc.Count = 4;							// anti-aliasing. max 4 min 1
-	swc_struct.Windowed = TRUE;									// Windowed or fullscreen
+	/* NOTE: fill the swap chain struct. 
+		https://learn.microsoft.com/en-us/windows/win32/api/dxgi/ns-dxgi-dxgi_swap_chain_desc
+		https://learn.microsoft.com/en-us/windows/win32/direct3ddxgi/dxgi-usage
+	*/
+	swc_struct.BufferCount = 1;									/* NOTE: one back buffer & one front buffer (in pair) */
+	swc_struct.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;	/* NOTE: color format used. Use 32 bit color */
+	swc_struct.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;	/* NOTE: swap chain behaviour flags. How swap chain to be used */
+	swc_struct.OutputWindow = hWnd;								/* NOTE: which window context to be used */
+	swc_struct.SampleDesc.Count = 4;							/* NOTE: anti-aliasing. max 4 min 1 */
+	swc_struct.Windowed = TRUE;									/* NOTE: Windowed or fullscreen */
 	swc_struct.BufferDesc.Width = SCREEN_WIDTH;
 	swc_struct.BufferDesc.Height = SCREEN_HEIGHT;
-	swc_struct.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;	// allow fullscreen mode
+	swc_struct.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;	/* NOTE: allow fullscreen mode */
 
 	// https://learn.microsoft.com/en-us/windows/win32/api/d3d11/nf-d3d11-d3d11createdeviceandswapchain
 	D3D11CreateDeviceAndSwapChain(
-		NULL,							// graphic adapter that we use
-		D3D_DRIVER_TYPE_HARDWARE,		// is it hardware or software rendering?
-		NULL,							// set the software code
-		NULL,							// flags
-		NULL,							// spesific hardware features
-		NULL,							// how manny feature that we want
-		D3D11_SDK_VERSION,				// version
-		&swc_struct,					// pointer to the swapchain
-		&swapchain,						// pointer of pointer to swapchain object
-		&dev,							// pointer of pointer to device object
-		NULL,							// pointer to feature level
-		&devcon							// pointer of pointer to device context object
+		NULL,							/* NOTE: graphic adapter that we use */
+		D3D_DRIVER_TYPE_HARDWARE,		/* NOTE: is it hardware or software rendering? */
+		NULL,							/* NOTE: set the software code */
+		NULL,							/* NOTE: flags */
+		NULL,							/* NOTE: spesific hardware features */
+		NULL,							/* NOTE: how manny feature that we want */
+		D3D11_SDK_VERSION,				/* NOTE: version */
+		&swc_struct,					/* NOTE: pointer to the swapchain */
+		&swapchain,						/* NOTE: pointer of pointer to swapchain object */
+		&dev,							/* NOTE: pointer of pointer to device object */
+		NULL,							/* NOTE: pointer to feature level */
+		&devcon							/* NOTE: pointer of pointer to device context object */
 	);
 
-	/* Get the address of the back buffer
+	/* NOTE:
+	* Get the address of the back buffer
 	* https://learn.microsoft.com/en-us/windows/win32/api/dxgi/nf-dxgi-idxgiswapchain-getbuffer
 	*/
-	ID3D11Texture2D* pBackBuffer{};												// texture/image to be rendered
-	swapchain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer); // find the backbuffer in swapchain and use it to create backbuffer texture object
+	ID3D11Texture2D* pBackBuffer{};												/* NOTE: texture/image to be rendered */
+	swapchain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer); /* NOTE: find the backbuffer in swapchain and use it to create backbuffer texture object */
 
-	/* Use the back buffer to create the render target */
-	dev->CreateRenderTargetView(pBackBuffer, NULL, &backBuffer);		// create the render target object
+	/* NOTE: Use the back buffer to create the render target */
+	dev->CreateRenderTargetView(pBackBuffer, NULL, &backBuffer);		/* NOTE: create the render target object */
 	pBackBuffer->Release();
 
-	/* set the render target as the back buffer */
-	devcon->OMSetRenderTargets(1, &backBuffer, NULL);					// set the render target to be used
+	/* NOTE: set the render target as the back buffer */
+	devcon->OMSetRenderTargets(1, &backBuffer, NULL);					/* NOTE: set the render target to be used */
 
-	/* set the viewport */
+	/* NOTE: set the viewport */
 	D3D11_VIEWPORT viewport{};
 	ZeroMemory(&viewport, sizeof(D3D11_VIEWPORT));
 
@@ -179,27 +185,26 @@ void InitDX3D(HWND hWnd)
 	viewport.Width = SCREEN_WIDTH;
 	viewport.Height = SCREEN_HEIGHT;
 
-	devcon->RSSetViewports(1, &viewport); // activate the viewport to be used
-
+	devcon->RSSetViewports(1, &viewport); /* NOTE: activate the viewport to be used */
 }
 
 void RenderFrame(void)
 {
-	devcon->ClearRenderTargetView(backBuffer, D3DXCOLOR(0.0f, 0.0f, 0.0f, 1.0f)); // fills the render target with a certain color
+	devcon->ClearRenderTargetView(backBuffer, D3DXCOLOR(0.0f, 0.0f, 0.0f, 1.0f)); /* NOTE: fills the render target with a certain color */
 
-	/* select which vertex buffer to be displayed */
+	/* NOTE: select which vertex buffer to be displayed */
 	UINT stride = sizeof(VERTEX);
 	UINT offset = 0;
 	devcon->IASetVertexBuffers(0, 1, &pVBuffer, &stride, &offset);
 
-	/* select primitive we are using */
-	//devcon->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_TRIANGLELIST); // shows as triangle
-	devcon->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_LINESTRIP);	// shows as line
+	/* NOTE: select primitive we are using */
+	//devcon->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_TRIANGLELIST); /* Note: shows as triangle */
+	devcon->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_LINESTRIP);	/* NOTE: shows as line */
 
-	/* draw the vertex buffer */
+	/* NOTE: draw the vertex buffer */
 	devcon->Draw(5, 0);
 
-	swapchain->Present(0, 0);	// "swap" the backbuffer to the frontbuffer to be shown
+	swapchain->Present(0, 0);	/* NOTE: "swap" the backbuffer to the frontbuffer to be shown */
 
 	//devcon->ClearRenderTargetView(backBuffer, D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f));
 	//swapchain->Present(0, 0);
@@ -209,14 +214,14 @@ void InitPipeline(void)
 {
 	ID3D10Blob* VS;
 	ID3D10Blob* PS;
-	/*
+	/* NOTE:
 	* https://learn.microsoft.com/en-us/windows/win32/direct3d11/d3dx11compilefromfile
 	* "Load the shaders1 file, then find the VShader function, compiles it as version 4.0 vertex shader and store the result in blob VS
 	*/
 	D3DX11CompileFromFile("shaders1.shader", 0, 0, "VShader", "vs_4_0", 0, 0, 0, &VS, 0, 0);
 	D3DX11CompileFromFile("shaders1.shader", 0, 0, "PShader", "ps_4_0", 0, 0, 0, &PS, 0, 0);
 
-	/*
+	/* NOTE:
 	* create the shader COM object
 	* 1st = address of the compiled shader/vertex
 	* 2nd = size of the file data
@@ -225,15 +230,11 @@ void InitPipeline(void)
 	dev->CreateVertexShader(VS->GetBufferPointer(), VS->GetBufferSize(), NULL, &pVS);
 	dev->CreatePixelShader(PS->GetBufferPointer(), PS->GetBufferSize(), NULL, &pPS);
 
-	/*
-	* set the shader to the active shader
-	*/
+	/* NOTE: set the shader to the active shader */
 	devcon->VSSetShader(pVS, 0, 0);
 	devcon->PSSetShader(pPS, 0, 0);
 
-	/*
-	* create input layout object
-	*/
+	/* NOTE: create input layout object */
 	D3D11_INPUT_ELEMENT_DESC ied[] =
 	{
 		{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0},
@@ -245,8 +246,9 @@ void InitPipeline(void)
 
 void InitGraphic(void)
 {
-	/* the triangle vertices */
-	/* locations/coordinates are in X,Y,Z */
+	/* NOTE: the triangle vertices
+			locations/coordinates are in X,Y,Z 
+	*/
 	VERTEX OurVertices[] =
 	{
 		//{0.0f, 0.5f, 0.9f, D3DXCOLOR(1.0f, 0.0f, 0.2f, 1.0f)},
@@ -261,7 +263,7 @@ void InitGraphic(void)
 		{-0.5f, 0.8f, 0.0f, D3DXCOLOR(1.0f, 0.0f, 0.0f, 1.0f)},
 	};
 
-	/* create vertex buffer */
+	/* NOTE: create vertex buffer, each vertex buffer can only handle one given points(?) */
 	D3D11_BUFFER_DESC bd{};
 	ZeroMemory(&bd, sizeof(bd));
 
@@ -280,9 +282,9 @@ void InitGraphic(void)
 
 void CleanDX3D(void)
 {
-	swapchain->SetFullscreenState(FALSE, NULL); // turn off the fullscreen
+	swapchain->SetFullscreenState(FALSE, NULL); /* NOTE: turn off the fullscreen */
 
-	/* release the shaders COM objects */
+	/* NOTE: release the shaders COM objects */
 	pVS->Release();
 	pPS->Release();
 
